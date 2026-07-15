2edb872ab921727740fc8494e826db56e1955c28
aymenbb7 <aymenbounehideja047@gmail.com>
2026-07-15 23:50:56 +0200

    fix: override admin login form to use email explicitly

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from .models import CustomUser
 
-# Register your models here.
+class EmailAuthenticationForm(AuthenticationForm):
+    username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'autofocus': True}))
+
+# Override the default admin login form globally
+admin.site.login_form = EmailAuthenticationForm
+
+# Register CustomUser if not already registered
+admin.site.register(CustomUser)
